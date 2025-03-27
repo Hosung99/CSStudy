8c8b01e51dc172cef3a710a74b9004b8099af1f4
soohalee <suha630@naver.com>
2025-03-27 23:54:11 +0900

    [feat] : 14주차 알고리즘

--- CodingTest/이수하/14주차/백트래킹/부분수열의합_1182.java
+++ CodingTest/이수하/14주차/백트래킹/부분수열의합_1182.java
@@ -4,58 +4,38 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
-    static int n, max;
-    static List<int[]> eggs;
+    static int n, s, cnt;
+    static int[] sequence;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer tk = new StringTokenizer(br.readLine());
 
-        eggs = new LinkedList<>();
         n = Integer.parseInt(tk.nextToken());
+        s = Integer.parseInt(tk.nextToken());
+
+        sequence = new int[n];
+        tk = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            tk = new StringTokenizer(br.readLine());
-            int life = Integer.parseInt(tk.nextToken());
-            int weight = Integer.parseInt(tk.nextToken());
-            eggs.add(new int[]{life, weight});
+            sequence[i] = Integer.parseInt(tk.nextToken());
         }
 
-        dfs(0);
+        dfs(0, 0, 0);
 
-        System.out.print(max);
+        System.out.println(cnt);
     }
 
-    static void dfs(int attack) {
-        if (attack == n) {
-            max = Math.max(cntBrokenEggs(), max);
-            return;
-        }
-
-        for (int defense = 0; defense < n; defense++) {
-            //공격할 계란과 맞아주는 계란이 같은 계란일 수는 없다.
-            if (defense == attack) continue;
-            //공격할 계란과 맞아주는 계란 중 하나라도 깨져있으면 아무것도 하지 않고 다음으로 넘어간다.
-            if (eggs.get(attack)[0] <= 0 || eggs.get(defense)[0] <= 0) {
-                dfs(attack + 1);
-                continue;
+    static void dfs(int idx, int sum, int selected) {
+        if (idx == n) {
+            if (s == sum && selected > 0) {
+                cnt++;
             }
-
-            eggs.get(attack)[0] -= eggs.get(defense)[1];
-            eggs.get(defense)[0] -= eggs.get(attack)[1];
-            dfs(attack + 1);
-            eggs.get(attack)[0] += eggs.get(defense)[1];
-            eggs.get(defense)[0] += eggs.get(attack)[1];
-
-        }
-    }
-
-    static int cntBrokenEggs() {
-        int res = 0;
-        for (int i = 0; i < n; i++) {
-            if (eggs.get(i)[0] <= 0) {
-                res++;
-            }
+            return ;
         }
 
-        return res;
+        // 현재 인덱스를 포함한다.
+        dfs (idx + 1, sum + sequence[idx], selected + 1);
+
+        // 현재 인덱스를 포함하지 않는다.
+        dfs (idx + 1, sum, selected);
     }
 }
